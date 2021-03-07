29c9b11f342ddd8abbb36d3ccf87e682fffcf1dc
Shannon Young <shannon.ari1@gmail.com>
2021-03-07 21:02:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,12 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
+
 
   def cobble
     self.condition = "new"
